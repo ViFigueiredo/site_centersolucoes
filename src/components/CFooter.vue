19759e0bc6c2ce934b7a0ec148a529ff53512e89
Vi Figueiredo <vifigcode@gmail.com.br>
2024-05-13 12:18:26 -0300

    add vvn selfit

--- src/components/CFooter.vue
+++ src/components/CFooter.vue
@@ -5,7 +5,7 @@
       <span>CPNJ: 08.645.168/0001-66</span>
       <span>Endereço: Av. Conselheiro Aguiar, 4599 - Boa Viagem/PE</span>
       <span>CEP: 51.021-020</span>
-      <span>Telefone: (81) 3341-5848</span>
+      <span>Telefone: (81) 3341-5848 / (81) 3048-1279</span>
       <span>E-mail: <span class="lowercase">FINANCEIRO@CENTERTELECOM.COM.BR</span></span>
     </div>
     <div class="flex flex-col items-end">
